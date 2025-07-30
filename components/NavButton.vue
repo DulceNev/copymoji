fd493d012c04c6d349c9c0242f0130908a570728
Dulce Nevarez <dulcenev04@gmail.com>
2025-07-30 16:41:26 -0500

    feat: update NavButton component and clean up index page layout

--- components/NavButton.vue
+++ components/NavButton.vue
@@ -9,6 +9,7 @@ interface Props {
 <template>
   <NuxtLink
     :to="props.to"
+    active-class="bg-primary text-primary-content"
     class="text-primary text-sm font-semibold cursor-pointer py-1 px-8 rounded-full hover:bg-primary hover:text-primary-content active:scale-95 transition-all"
   >
     {{ props.text }}
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,5 @@
+<script setup>
+</script>
 <template>
   <main class="h-full flex flex-col items-center text-center gap-5 py-5">
     <h1 class="text-secondary">
@@ -23,16 +25,11 @@
       </div>
 
       <section>
-        <div
-          class="tooltip tooltip-primary"
-          data-tip="como se ponen iconos aqui aa"
+        <button
+          class="noto-sans text-secondary border-1 rounded-lg p-2 text-sm"
         >
-          <button
-            class="noto-sans text-secondary border-1 rounded-lg p-2 text-sm"
-          >
-            (˶>⩊<˶)
-          </button>
-        </div>
+          (˶>⩊<˶)
+        </button>
       </section>
     </div>
   </main>
